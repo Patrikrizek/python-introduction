740d3bfb9f522b60eac0a9f20e25563b7bbc9034
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-09-23 14:45:56 +0100

    introduction
    data type, variables, expressions, naming variables, converting String to Int

---
+++ intro.py
@@ -0,0 +1,31 @@
+# Print and data type
+print('Hello, world!')
+print(378 * 183)
+
+# Varibles
+pi = 3.14
+name = 'Patrik'
+print(pi)
+print(name)
+
+# Expressions
+# bodmas, bomdas, pedmas, bidmas
+number =( 1 + 2 + 3) * 4
+print(number)
+
+# Naming Variables
+# meaningful names
+biology = 60
+chemistry = 70
+physics = 80
+total = biology + chemistry + physics
+print(total)
+
+first_name = "Patrik"
+  
+# Converting String to Int while using input() function
+biology = int(input("Please enter your Biology marks: "))
+chemistry = int(input("Please enter your Chemistry marks: "))
+physics = int(input("Please enter your Physics marks: "))
+total = biology + chemistry + physics
+print(total)
