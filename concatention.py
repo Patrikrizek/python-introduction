e34e2ef035ec6b6c128cc03a7a37cda251cc4f14
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-09-24 12:36:30 +0100

    concatention

---
+++ concatention.py
@@ -0,0 +1,26 @@
+# Concatention
+result1 = "1" + "2" + "3"
+# Int, float
+result2 = 1 + 2 + 3
+
+print(result1)
+print(result2)
+
+# Boolean
+print(10 > 5)
+
+result3 = 1 - 2 - 3
+# result4 = "1" - "2" - "3" >>> Error
+print(result3)
+# print(result4)
+
+# Data type
+print(type(result1))
+print(type(result2))
+
+name = "Patrik"
+
+print(name.upper())
+print(name.lower())
+
+print('my \n name \n is \n \t\t Python')
