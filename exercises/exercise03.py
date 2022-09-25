95e01cc8fe89683ba063edf84c9048a65c16ee3d
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-09-25 14:36:56 +0100

    exercise

---
+++ exercises/exercise03.py
@@ -0,0 +1,10 @@
+def exercise_three():
+  number1 = input('Enter whole number:')
+  number2 = input('Enter decimal number:')
+
+  int_num = int(number1)
+  float_num = float(number2)
+  round_num = int(round(float_num))
+
+  print(int_num, float_num, round_num)
+exercise_three()
