33d3ee5e30e94bb47458530935ff1a4924502c29
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-09-26 12:48:03 +0100

    exercise

---
+++ exercises/exercise05.py
@@ -0,0 +1,9 @@
+def flowcharts():
+  zero = 0
+  num = int(input('Please enter a number:'))
+  if num % 2 == zero:
+    print(num, ' is even')
+  else:
+    print(num, ' is odd')
+
+flowcharts()
---
+++ exercises/exercise06.py
@@ -0,0 +1,10 @@
+def exercise_one():
+  password = 'Hello World!'
+  user = input('Please enter your password: ')
+
+  if password == user:
+    print('Print is correct')
+  else:
+    print('ERROR: Try again')
+
+exercise_one()  
